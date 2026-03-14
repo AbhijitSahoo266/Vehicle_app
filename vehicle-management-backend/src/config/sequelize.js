871e9f40f32c35993c5cd9bcd7467f1463b76e9a
Abhijit Sahoo <abhijitsahoo266@gmail.com>
2026-03-14 22:18:20 +0530

    sequlizy change

--- vehicle-management-backend/src/config/sequelize.js
+++ vehicle-management-backend/src/config/sequelize.js
@@ -12,12 +12,19 @@
 //   }
 // );
 
-// module.exports = sequelize;
+// module.exports = sequelize; 
+
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   logging: false,
 });
 
